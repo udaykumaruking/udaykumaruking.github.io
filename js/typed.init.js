8c036abb4a3f48a4c222e1c91620030c194f4381
Uday Kumar Madarapu <udaykumaruking@gmail.com>
2021-06-14 21:52:03 +0530

    Done with Hero Section, personal Details, What I Do

--- js/typed.init.js
+++ js/typed.init.js
@@ -1,10 +1,12 @@
-
 // Typed Text
 var typed = new Typed('#typed', {
-    stringsElement: '#typed-strings',
-    typeSpeed: 200,
-    loop: true,
-    strings: [
-        'Web Designer', 'Web Developer', 'Photographer',
-    ],
+  stringsElement: '#typed-strings',
+  typeSpeed: 200,
+  loop: true,
+  strings: [
+    'Web Designer',
+    'Graphic Designer',
+    'UI/UX Designer',
+    'App Developer',
+  ],
 });
